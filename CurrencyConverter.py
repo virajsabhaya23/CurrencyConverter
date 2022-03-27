a2b10f2cd5740ebb88075a2fa648bc87b87c14d4
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2022-03-26 21:40:13 -0500

    get_currencies function added+working

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,2 +1,43 @@
-BASE_URL =  'https://free.currconv.com'
+from requests import get
+from pprint import PrettyPrinter
+
+BASE_URL =  "https://free.currconv.com/"
 API_KEY = "970b760dfd21aa95d8ed"
+
+printer = PrettyPrinter()
+
+def get_currencies():
+    endpoint = f"api/v7/currencies?apiKey={API_KEY}"
+    url = BASE_URL + endpoint
+    data = get(url).json()['results']
+
+    data=list(data.items())
+    data.sort()
+
+    return data
+
+
+def print_currencies(currencies):
+    for name, currency in currencies:
+        name = currency['currencyName']
+        _id_ = currency['id']
+        symbol = currency.get("currencySymbol","")
+        print(f"{_id_} - {name} - {symbol}")
+
+
+def exchange_rate(currency1, currency2):
+    endpoint = f"api/v7/convert?q={currency1}_{currency2}&compact=ultra&apiKey={API_KEY}"
+    url = BASE_URL + endpoint
+    data = get(url).json()
+    
+    if len(data) == 0:
+        print("invalid currencies.")
+        return
+
+    return list(data.values())[0]
+
+
+# data = get_currencies()
+# print_currencies(data)
+rate = exchange_rate("USD","CAD")
+print(rate)
--- README.md
+++ README.md
@@ -4,3 +4,21 @@ This is a simple python program to convert currencies using the online API calle
 - *NOTE*: This is a free version thus the rates are updated once every 60 mins.
 
 
+---
+
+### Requirements : 
+<p>
+   <img src="https://img.shields.io/badge/python-v3.6.0-brown ">
+    <img src="https://img.shields.io/badge/pip-v22.0.4-blueviolet ">
+    <img src="https://img.shields.io/badge/requests-pip%2Fpip3-ff69b4 ">
+    <img src="https://img.shields.io/badge/pprint-pip%2Fpip3-orange ">
+    <br />
+    <a href="https://free.currencyconverterapi.com"><img src="https://img.shields.io/badge/API-CurrencyConverter-blue "></a>
+    <br />
+</p>
+
+- *NOTE*: make sure to verify your email-address if you generate the API_KEY.
+
+---
+
+
